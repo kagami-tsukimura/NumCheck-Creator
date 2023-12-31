3440efc2bdd25a61b81ca7f88439484602fa537a
Kagami <shimura.kyohei7617@gmail.com>
2023-12-31 17:26:08 +0900

    refactor: input number

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
     <>
       <div className='material-input-container'>
         <input
-          type='text'
+          type='number'
           value={number}
           onChange={handleInputChange}
           placeholder='Enter a number'
