680338100c7cf3b5fd8faebf4b3880fe5854db37
Kagami <shimura.kyohei7617@gmail.com>
2023-12-31 16:56:24 +0900

    Initialize repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App: React.FC = () => {
+  const [number, setNumber] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputNumber = event.target.value;
+    setNumber(inputNumber);
+  };
+
+  const renderCheckboxes = (): JSX.Element[] => {
+    const checkboxes: JSX.Element[] = [];
+    const maxDigitCount =
+      number.length > 0
+        ? Math.max(...Array.from(number).map((digit) => parseInt(digit, 10)))
+        : 1;
+
+    for (let i = 1; i <= parseInt(number, 10); i++) {
+      const labelContent = `${i}`;
+
+      checkboxes.push(
+        <div
+          key={i}
+          style={{
+            display: 'inline-block',
+            boxSizing: 'border-box',
+            textAlign: 'center',
+            // 1行最大10個のチェックボックス
+            width: `${100 / 10}%`,
+            minWidth: `${maxDigitCount * 20}px`,
+            whiteSpace: 'nowrap',
+            // 行間
+            marginBottom: '1em',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <label htmlFor={`checkbox-${i}`} style={{ fontSize: '1.5em' }}>
+            {labelContent}
+          </label>
+          <br />
+          <input
+            type='checkbox'
+            id={`checkbox-${i}`}
+            style={{ transform: 'scale(2)' }}
+          />
+        </div>
+      );
+
+      if (i % 10 === 0) {
+        checkboxes.push(<br key={`br-${i}`} />);
+      }
+    }
+
+    return checkboxes;
+  };
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={number}
+        onChange={handleInputChange}
+        placeholder='Enter a number'
+      />
+      <div>{renderCheckboxes()}</div>
     </div>
   );
-}
+};
 
 export default App;
